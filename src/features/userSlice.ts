6e58ad21a9020f9de74f93ceea3c2a11fe91d9c1
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-04-08 18:45:07 +0900

    update

---
+++ src/features/userSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { RootState } from '../app/store'
+
+export type UserState = {
+  user: {
+    uid: string | undefined
+    photoUrl: string | undefined
+    displayName: string | undefined
+  }
+}
+
+const initialState: UserState = {
+  user: {
+    uid: undefined,
+    photoUrl: undefined,
+    displayName: undefined,
+  },
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload
+    },
+    logout: (state) => {
+      state.user = initialState.user
+    },
+    updateUserProfile: (state, action: PayloadAction<UserState>) => {
+      state.user.displayName = action.payload.user.displayName
+      state.user.photoUrl = action.payload.user.photoUrl
+    },
+  },
+})
+
+export const { login, logout } = userSlice.actions
+
+export const selectUser = (state: RootState) => state.user.user
+
+export default userSlice.reducer
