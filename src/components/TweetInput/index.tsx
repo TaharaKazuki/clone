f70141541fe70114afe723a52674ec25ba431e1f
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-04-21 22:44:57 +0900

    update

--- src/components/TweetInput/index.tsx
+++ src/components/TweetInput/index.tsx
@@ -3,14 +3,26 @@ import { storage, db, auth } from '../../firebase'
 import { useSelector } from 'react-redux'
 import { selectUser } from '../../features/userSlice'
 import { Avatar, Button, IconButton } from '@material-ui/core'
+import firebase from 'firebase/app'
 import AddAPhotoIcon from '@material-ui/icons/AddAPhoto'
+import styles from './style.module.css'
 
 const TweetInput = () => {
   const user = useSelector(selectUser)
   const [tweetImage, setTweetImage] = useState<File | null>(null)
   const [tweetMsg, setTweetMsg] = useState('')
 
-  return <div>index</div>
+  return (
+    <>
+      <Avatar
+        className={styles.tweet_avatar}
+        src={user.photoUrl}
+        onClick={async () => {
+          await auth.signOut()
+        }}
+      />
+    </>
+  )
 }
 
 export default TweetInput
